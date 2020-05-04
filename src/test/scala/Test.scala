cafaddb85f4a92c5879bc06e2841cdafc6a2650f
fra.cassano2 <francesco.cassano2@studio.unibo.it>
2020-05-05 00:32:40 +0200

    test sbagliato

--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -1,1 +1,12 @@
-class Test
+package testset
+
+import org.scalatest.FunSuite
+import org.scalatest.matchers.ShouldMatchers
+import prova.Main
+
+class ProvaTest extends FunSuite with ShouldMatchers {
+
+  test("Fallisci"){
+    Main.combination(List("fra", "fab", "gia"), List("dio")).size should equal(4)
+  }
+}
