51333b8f716d4dfd050c3a09ec885dcba4867c29
ffracas <61873436+ffracas@users.noreply.github.com>
2020-05-05 01:32:17 +0200

    correzione

--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -1,18 +1,5 @@
-<<<<<<< HEAD
-package testset
-
-import org.scalatest.FunSuite
-import org.scalatest.matchers.ShouldMatchers
-import prova.Main
-
-class ProvaTest extends FunSuite with ShouldMatchers {
-
-  test("Fallisci"){
-    Main.combination(List("fra", "fab", "gia"), List("dio")).size should equal(4)
-  }
-
-
 package test.scala
+
 import org.scalatest._
 import prova.Main._
 
